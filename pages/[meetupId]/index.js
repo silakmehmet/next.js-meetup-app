1b3455a05e42491cedffac188de7acc4551a2fba
Mehmet Şılak <silakmehmet@gmail.com>
2023-08-11 13:33:50 +0300

    Dynamic routing added for meetup details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails(props) {
+  return (
+    <MeetupDetail
+      image="https://picsum.photos/200/300"
+      title="The First Meetup"
+      address="Atakule no:65"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
